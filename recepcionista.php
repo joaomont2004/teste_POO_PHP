cef7549faa2497716ee058d3c5cf571160cde359
João Gabriel Borges Monteiro <joaomont2004@gmail.com>
2026-09-08 10:22:47 -0300

    classe atendente

---
+++ recepcionista.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once 'funcionario.php';
+
+class recepcionista extends funcionario {
+    private string $turno;
+    private string $folga;
+    
+    public function __construct(string $nome, string $cpf, string $tele, string $data, string $cargo, float $salario, string $turno, string $folga) {
+        parent::__construct($nome, $cpf, $tele, $data, $cargo, $salario);
+        $this->turno = $turno;
+        $this->folga = $folga;
+    }
+
+
+    public function descricao() {
+        return parent::descricao() . "\n" . "TURNO: $this->turno" . "\n" . "FOLGA: $this->folga";
+    }
+}
